22344fa80e3bb75c4779dafd763d14b3f039aac3
Daveberry <84567826+daveberrys@users.noreply.github.com>
2026-03-08 20:32:02 +0800

    fix icon not being found & organize

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const path = require('path')
 const app = express()
 const port = 34537 // which stans for "files" :3
 
-app.use(express.static(path.join(process.cwd(), 'public')));
 app.use(express.static(path.join(process.cwd(), 'src')));
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'src', 'index.html'))
 })
